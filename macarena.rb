10781b7a67fb53f6df729223817d3df693e2faa2
srappaport <sarah.rappaport@greendot.org>
2018-07-09 21:16:01 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -18,7 +18,7 @@ def macarena
   puts "Step 1: Right arm out in front of you, palm facing down."
   puts "Step 2: Left arm out in front of you, palm facing down"
   puts "Step 3: Turn your right palm up"
-  puts "Step 4: Turn your left palm up."
+  puts "Step 4: Turn your left palm."
   puts "Step 5: Place your right hand on your left shoulder"
   puts "Step 6: Place your left hand on your right shoulder"
   puts "Step 7: Bring your right hand up and around to the back of your head"
