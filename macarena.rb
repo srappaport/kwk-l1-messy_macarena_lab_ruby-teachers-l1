cb81773f5702c1a87a9342888a65e223e7a8dc79
srappaport <sarah.rappaport@greendot.org>
2018-07-09 19:43:51 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -16,6 +16,7 @@ def macarena
   puts "Step 7: Bring your right hand up and around to the back of your head"
   puts "Step 8: Bring your left hand up and around to the back your head"
   x=10
+  y=100
 end
 
 def hello
